aeaf09270ec637c9e9b467245814a542cd232605
harrywoodhouse <woodhouses.harry@gmail.com>
2026-04-21 14:23:42 +0100

    Added custom error handling template

--- crates/dcca-engine/src/lib.rs
+++ crates/dcca-engine/src/lib.rs
@@ -12,3 +12,32 @@ PURPOSE. See the GNU General Public License for more details.
 You should have received a copy of the GNU General Public License along with 
 DeepCorr. If not, see <https://www.gnu.org/licenses/>.
 */
+
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum TensorError {
+    #[error("Input data is empty")]
+    EmptyInput,
+}
+
+impl TensorError {
+    pub fn code(&self) -> &str {
+        match self {
+            NormError::EmptyInput => "T101",
+        }
+    }
+
+    pub fn wiki_url(&self) -> String {
+        format!("https://github.com/mosaic-rs/deepcorr/wiki/Errors#{}", self.code())
+    }
+
+    pub fn formatted_message(&self) -> String {
+        format!(
+            "[{}] {}\nWiki: {}",
+            self.code(),
+            self,
+            self.wiki_url()
+        )
+    }
+}
